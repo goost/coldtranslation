099b744e7b7a456f7346099da5b0c3c37a714098
Gleb Ostrowski <glebos@web.de>
2018-12-09 23:07:50 +0100

    Line Ending errors

--- ColdTranslation.Views/Properties/Resources.Designer.cs
+++ ColdTranslation.Views/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ColdTranslation.Properties {
+namespace ColdTranslation.Views.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ColdTranslation.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ColdTranslation.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ColdTranslation.Views.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
